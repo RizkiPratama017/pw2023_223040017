10e89b31ad2ee003f08399f8eb20c8231f57e5ca
RizkiPratama017 <rizki.223040017@mail.unpas.ac.id>
2023-02-19 23:44:37 +0700

    tugas 1 selesai

---
+++ kuliah/pertemuan2/index.php
@@ -0,0 +1,25 @@
+<?php
+$nama = 'Rizki';
+$hari = 'Hari jum\'at kita "ujian"';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pertemuan 2</title>
+</head>
+
+<body>
+    <h1>
+        <?php echo "Hello, $nama!"; ?>
+    </h1>
+    <p><?php echo '"Pemrograman Web"' ?></p>
+    <p><?php echo "$hari" ?></p>
+    <p></p>
+
+</body>
+
+</html>
---
+++ tugas/tugas1/1b.php
@@ -0,0 +1,20 @@
+<?php $absen = 17; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <p>Aku adalah angka <b><?php echo $absen ?></b></p>
+    <p>Jika aku dikali 5, maka aku sekarang menjadi <b><?php echo $absen * 5; ?></b></p>
+    <p>Jika aku dibagi 2, maka aku sekarang menjadi <b><?php echo $absen / 2; ?></b></p>
+    <p>Jika aku ditambah 75, maka aku sekarang menjadi <b><?php echo $absen + 75; ?></b></p>
+    <p>Jika aku dikurang 20, maka aku sekarang menjadi <b><?php echo $absen - 20; ?></b></p>
+</body>
+
+</html>
