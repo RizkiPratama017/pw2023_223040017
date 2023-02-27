c190321622bfe1252eafed3749e1b03629ec2ae9
RizkiPratama017 <rizki.223040017@mail.unpas.ac.id>
2023-02-27 09:07:37 +0700

    revisi Tugas 2a

--- tugas/tugas2/2a.php
+++ tugas/tugas2/2a.php
@@ -3,16 +3,15 @@ $nama_depan = 'Rizki';
 $nama_belakang = 'Pratama';
 
 for ($nilai_awal = 1; $nilai_awal <= 100; $nilai_awal++) {
-    if ($nilai_awal % 3 == 0) {
-        echo "$nama_depan ";
+    if ($nilai_awal % 3 == 0 && $nilai_awal % 5 == 0) {
+        echo "$nama_depan $nama_belakang ";
+    } elseif ($nilai_awal % 3 == 0) {
+        echo "$nama_depan";
+    } elseif ($nilai_awal % 5 == 0) {
+        echo "$nama_belakang";
     } else {
         echo "$nilai_awal";
     }
-    if ($nilai_awal % 5 == 0) {
-        echo "$nama_belakang";
-    }
-    if ($nilai_awal % 5 == 0 && $nilai_awal % 3 == 0) {
-        echo "$nama_depan $nama_belakang";
-    }
+
     echo "<br>";
 }
